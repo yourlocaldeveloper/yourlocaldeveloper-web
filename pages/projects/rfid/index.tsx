74198e00a1cc6065647eedb654c71b185c8de52f
yourlocaldeveloper <aeroyts@icloud.com>
2024-10-09 17:39:29 +0300

    feat: add HomeView background svg and update rfid project page content

--- pages/projects/rfid/index.tsx
+++ pages/projects/rfid/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { SingleProjectView } from '@/components/organisms/SingleProjectView';
 
 import rfid from '@/app/rfid.png';
+import Link from 'next/link';
 
 const RfidPage: React.FC = ({}) => {
   return (
@@ -21,7 +22,24 @@ const RfidPage: React.FC = ({}) => {
         'OBS',
       ]}
     >
-      Details Coming Soon
+      A live demo video and overview of the project is coming soon. In the
+      meantime, please check out the GitHub repos that I have now made public:
+      <br />
+      RFID Table Controller - React Native App:{' '}
+      <Link
+        href={'https://github.com/yourlocaldeveloper/thelocalgame_live_app'}
+        target="_blank"
+      >
+        GitHub
+      </Link>
+      <br />
+      Stream Overlay SPA & Back-end - Next.js & Express App:{' '}
+      <Link
+        href={'https://github.com/yourlocaldeveloper/thelocalgame'}
+        target="_blank"
+      >
+        GitHub
+      </Link>
     </SingleProjectView>
   );
 };
